b99e69d32ac562760697fbf372d16b4162ff1803
EQZIP <10509759@qq.com>
2021-02-24 15:47:34 +0800

    Update comments...

--- src/main/java/org/eqcoin/passport/storage/PassportStateVariable.java
+++ src/main/java/org/eqcoin/passport/storage/PassportStateVariable.java
@@ -1,11 +1,11 @@
 /**
- * EQcoin core - EQcoin Federation's EQcoin core library
+ * EQcoin core
  *
  * http://www.eqcoin.org
  *
- * @copyright 2018-present EQcoin Federation All rights reserved...
- * Copyright of all works released by EQcoin Federation or jointly released by
- * EQcoin Federation with cooperative partners are owned by EQcoin Federation
+ * @copyright 2018-present EQcoin Planet All rights reserved...
+ * Copyright of all works released by EQcoin Planet or jointly released by
+ * EQcoin Planet with cooperative partners are owned by EQcoin Planet
  * and entitled to protection available from copyright law by country as well as
  * international conventions.
  * Attribution — You must give appropriate credit, provide a link to the license.
@@ -13,10 +13,10 @@
  * No Derivatives — If you remix, transform, or build upon the material, you may
  * not distribute the modified material.
  * For any use of above stated content of copyright beyond the scope of fair use
- * or without prior written permission, EQcoin Federation reserves all rights to
- * take any legal action and pursue any right or remedy available under applicable
+ * or without prior written permission, EQcoin Planet reserves all rights to take 
+ * any legal action and pursue any right or remedy available under applicable
  * law.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
