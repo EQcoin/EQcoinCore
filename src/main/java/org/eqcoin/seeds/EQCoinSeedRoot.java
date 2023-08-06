e0dcf7f6d50180701b84853fbfdb30ade3e4bee6
eqzip <10509759@qq.com>
2023-08-06 21:08:22 +0800

    Update copyright...

--- src/main/java/org/eqcoin/seeds/EQCoinSeedRoot.java
+++ src/main/java/org/eqcoin/seeds/EQCoinSeedRoot.java
@@ -3,24 +3,24 @@
  *
  * http://www.eqcoin.org
  * 
- * @Copyright 2018-present Wandering Earth Corporation All Rights Reserved...
- * The copyright of all works released by Wandering Earth Corporation or jointly
- * released by Wandering Earth Corporation with cooperative partners are owned
- * by Wandering Earth Corporation and entitled to protection available from
- * copyright law by country as well as international conventions.
+ * @Copyright 2018-present Xun Wang All Rights Reserved...
+ * The copyright of all works released by Xun Wang or jointly released by Xun
+ * Wang with cooperative partners are owned by Xun Wang and entitled to
+ * protection available from copyright law by country as well as international
+ * conventions.
  * Attribution — You must give appropriate credit, provide a link to the license.
  * Non Commercial — You may not use the material for commercial purposes.
  * No Derivatives — If you remix, transform, or build upon the material, you may
  * not distribute the modified material.
- * Wandering Earth Corporation reserves any and all current and future rights, 
- * titles and interests in any and all intellectual property rights of Wandering Earth 
- * Corporation, including but not limited to discoveries, ideas, marks, concepts, 
- * methods, formulas, processes, codes, software, inventions, compositions, techniques, 
- * information and data, whether or not protectable in trademark, copyrightable 
- * or patentable, and any trademarks, copyrights or patents based thereon.
- * For the use of any and all intellectual property rights of Wandering Earth Corporation 
- * without prior written permission, Wandering Earth Corporation reserves all 
- * rights to take any legal action and pursue any rights or remedies under applicable law.
+ * Xun Wang reserves any and all current and future rights, titles and interests
+ * in any and all intellectual property rights of Xun Wang including but not limited
+ * to discoveries, ideas, marks, concepts, methods, formulas, processes, codes,
+ * software, inventions, compositions, techniques, information and data, whether
+ * or not protectable in trademark, copyrightable or patentable, and any trademarks,
+ * copyrights or patents based thereon. For the use of any and all intellectual
+ * property rights of Xun Wang without prior written permission, Xun Wang
+ * reserves all rights to take any legal action and pursue any rights or remedies
+ * under applicable law.
  */
 //package org.eqcoin.seeds;
 //
